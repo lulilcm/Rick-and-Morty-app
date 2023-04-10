e4abd11e5f5ee5e7d157a64af6e86fc07849e2eb
Lourdes Moreno <lourdescandelamoreno@gmail.com>
2023-04-10 19:40:57 -0300

    validation fixed

--- client/src/components/Form/validation.js
+++ client/src/components/Form/validation.js
@@ -1,24 +1,32 @@
 const validateData =  (userData, errors, setErrors) => {
   // username
-  if (!userData.email) 
+  if (!userData.email) {
     setErrors({ ...errors, email:'Completar este campo'});
-  else if (userData.email.length > 35) 
+  }
+    
+  else if (userData.email.length > 35)  {
     setErrors({ ...errors, username:'No puede sobrepasar los 35 caracteres' });
-  else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{3})+$/.test(userData.username)){
-    setErrors({ ...errors, username:'Email invalido' });
-  } 
-  else{
-    setErrors({ ...errors, email:'' });
+  }
+   
+  else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{3})+$/.test(userData.username)) {
+     setErrors({ ...errors, username:'Email invalido' });
+  }
+  
+    // password
+  else if (userData.password.length < 6 || userData.password.length > 10 ) {
+    setErrors({ ...errors, password:'Longitud erronea'});
+  }
+    
+  else if(!/\d/.test(userData.password)) {
+    setErrors({ ...errors, password:'Debe contener al menos un numero'});
   }
 
-  // password
-  if (userData.password.length < 6 || userData.password.length > 10 ){
-    setErrors({ ...errors, password:'Longitud erronea'});
-  }else if(!/\d/.test(userData.password)) {
-    setErrors({ ...errors, password:'Debe contener al menos un numero'});
-  }else{
-    setErrors({ ...errors, password:'' });
+  else {
+   setErrors({ ...errors, email:'', password:'' });
   }
+    
+  
+  
   
 };
 
